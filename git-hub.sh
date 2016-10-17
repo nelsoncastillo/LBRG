f33c6f5ea32b37f1f3d922f6643921435d2ad5ad
Nelson Castillo <ingeniero.nelson.castillo@gmail.com>
2016-10-16 20:28:34 -0400

    first commit UELibertador

--- git-hub.sh
+++ git-hub.sh
@@ -11,21 +11,20 @@ echo git status
 echo git commit -m "HTML"
  
  
-echo "# Liceo Bolivariano RG" 
+ echo "# UELibertador" 
 git init
 git add .
-git commit -m "html y fuentes LBRG"
-git remote add origin https://github.com/nelsoncastillo/LBRG.git
+git commit -m "first commit UELibertador"
+git remote add origin https://github.com/nelsoncastillo/UELibertador.git
 git push -u origin master
  
  
 echo git remote add origin https://github.com/nelsoncastillo/UELibertador
 echo git remote add origin git@github.com:nelsoncastillo/UELibertador
 echo git push origin master
-echo git pull
-echo git pull -u origin 
+echo --git pull
+echo --git pull -u origin 
 echo si falla a la primera forzar con merge
-
 git fetch origin
 
 git merge origin/master
